22b57fb320739c1d2d2e03e01afdf02f4c3cfedf
Rishi Bhalla <rishi@illogical.in>
2025-09-22 12:33:53 +0530

    feat: added a new file with the colors

---
+++ colors.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	TextHighlight      = lipgloss.Color("#f9e2af") // Yellow
+	TitleBackground    = lipgloss.Color("#89b4fa") // Blue
+	TitleForeground    = lipgloss.Color("#1e1e2e") // Dark text
+	SubtitleBackground = lipgloss.Color("#cba6f7") // Mauve
+	SubtitleForeground = lipgloss.Color("#1e1e2e") // Dark text
+	FooterGrey         = lipgloss.Color("#6c7086") // Overlay0
+	ListSelected       = lipgloss.Color("#a6e3a1") // Green
+	GenericText        = lipgloss.Color("#cdd6f4") // Text
+	ErrorText          = lipgloss.Color("#f38ba8") // Red
+	Cursor             = lipgloss.Color("#f5e0dc") // Rosewater
+)
